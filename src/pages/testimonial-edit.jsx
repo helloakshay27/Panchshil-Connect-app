c4dc78f648b0754fdfcad520bebe46a39f2a4542
OnkarMotewar <onkarmotewar29@gmail.com>
2025-03-05 14:11:12 +0530

    testimonial

--- src/pages/testimonial-edit.jsx
+++ src/pages/testimonial-edit.jsx
@@ -80,12 +80,12 @@ const TestimonialEdit = () => {
     <div className="main-content">
       <div className="website-content overflow-auto">
         <div className="module-data-section p-3">
-          <div className="card mt-4 pb-4 mx-4">
-            <div className="card-header">
-              <h3 className="card-title">Edit Testimonial</h3>
-            </div>
-            <div className="card-body">
-              <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
+            <div className="card mt-5 pb-4 mx-4">
+              <div className="card-header">
+                <h3 className="card-title">Edit Testimonial</h3>
+              </div>
+              <div className="card-body">
                 <div className="row">
                   <div className="col-md-3">
                     <div className="form-group">
@@ -174,29 +174,29 @@ const TestimonialEdit = () => {
                     </div>
                   </div>
                 </div>
-                <div className="row mt-2 justify-content-center">
-                  <div className="col-md-2 mt-3">
-                    <button
-                      type="submit"
-                      className="purple-btn2 w-100"
-                      disabled={loading}
-                    >
-                      Submit
-                    </button>
-                  </div>
-                  <div className="col-md-2 mt-3">
-                    <button
-                      type="button"
-                      className="purple-btn2 w-100"
-                      onClick={handleCancel}
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                </div>
-              </form>
+              </div>
             </div>
-          </div>
+            <div className="row mt-2 justify-content-center">
+              <div className="col-md-2 mt-3">
+                <button
+                  type="submit"
+                  className="purple-btn2 w-100"
+                  disabled={loading}
+                >
+                  Submit
+                </button>
+              </div>
+              <div className="col-md-2 mt-3">
+                <button
+                  type="button"
+                  className="purple-btn2 w-100"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
--- src/pages/testimonials.jsx
+++ src/pages/testimonials.jsx
@@ -108,12 +108,12 @@ const Testimonials = () => {
       <div className="main-content">
         <div className="website-content overflow-auto">
           <div className="module-data-section p-3">
-            <div className="card mt-4 pb-4 mx-4">
-              <div className="card-header">
-                <h3 className="card-title">Create Testimonials</h3>
-              </div>
-              <div className="card-body">
-                <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit}>
+              <div className="card mt-5 pb-4 mx-4">
+                <div className="card-header">
+                  <h3 className="card-title">Create Testimonials</h3>
+                </div>
+                <div className="card-body">
                   <div className="row">
                     <div className="col-md-3">
                       <div className="form-group">
@@ -223,29 +223,29 @@ const Testimonials = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="row mt-2 justify-content-center">
-                    <div className="col-md-2">
-                      <button
-                        type="submit"
-                        className="purple-btn2 w-100"
-                        disabled={loading}
-                      >
-                        Submit
-                      </button>
-                    </div>
-                    <div className="col-md-2">
-                      <button
-                        type="button"
-                        className="purple-btn2 w-100"
-                        onClick={handleCancel}
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  </div>
-                </form>
+                </div>
               </div>
-            </div>
+              <div className="row mt-2 justify-content-center">
+                <div className="col-md-2">
+                  <button
+                    type="submit"
+                    className="purple-btn2 w-100"
+                    disabled={loading}
+                  >
+                    Submit
+                  </button>
+                </div>
+                <div className="col-md-2">
+                  <button
+                    type="button"
+                    className="purple-btn2 w-100"
+                    onClick={handleCancel}
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
