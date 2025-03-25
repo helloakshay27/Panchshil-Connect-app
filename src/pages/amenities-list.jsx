d6ca2fe7513f6c835c0cac67b10904d08d59d740
OnkarMotewar <onkarmotewar29@gmail.com>
2025-03-25 13:16:47 +0530

    added delete button amenities

--- src/pages/amenities-list.jsx
+++ src/pages/amenities-list.jsx
@@ -72,6 +72,25 @@ const AmenitiesList = () => {
     setSearchQuery(e.target.value);
     setPagination((prevState) => ({ ...prevState, current_page: 1 }));
   };
+  const handleDelete = async (id) => {
+    const confirmDelete = window.confirm("Are you sure you want to delete this amenity?");
+    if (!confirmDelete) return;
+  
+    try {
+      await axios.delete(`https://panchshil-super.lockated.com/amenity_setups/${id}.json`);
+      toast.success("Amenity deleted successfully!");
+  
+      setAmenities((prevAmenities) => prevAmenities.filter((amenity) => amenity.id !== id));
+      setPagination((prevState) => ({
+        ...prevState,
+        total_count: prevState.total_count - 1,
+        total_pages: Math.ceil((prevState.total_count - 1) / pageSize),
+      }));
+    } catch (error) {
+      toast.error("Error deleting amenity. Please try again.");
+    }
+  };
+  
 
   const handleSearchSubmit = (event) => {
     event.preventDefault();
@@ -259,7 +278,7 @@ const AmenitiesList = () => {
                                 </button>
                               </td>
 
-                              <td style={{ textAlign: "left" }}>
+                              <td >
                                 <button
                                   className="btn btn-link"
                                   onClick={() =>
@@ -293,6 +312,30 @@ const AmenitiesList = () => {
                                     />
                                   </svg>
                                 </button>
+                                <button
+                                className=" btn-link"
+                                onClick={() => handleDelete(amenity.id)}
+                                style={{
+                                  background: "none",
+                                    border: "none",
+                                    padding: "0",
+                                    display: "flex",
+                                    alignItems: "center",
+                                    justifyContent: "flex-start",
+                                }}
+                              >
+                                <svg
+                                  xmlns="http://www.w3.org/2000/svg"
+                                  width="16"
+                                  height="16"
+                                  fill="currentColor"
+                                  class="bi bi-trash3"
+                                  viewBox="0 0 16 16"
+                                >
+                                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                                </svg>
+                              </button>
+
                               </td>
                             </tr>
                           ))
