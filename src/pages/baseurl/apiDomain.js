765f72dcc1fd5672b4328558eab7133f4d174935
helloakshay27 <helloakshay27@gmail.com>
2025-06-17 23:13:01 +0530

    Refactor baseURL assignment logic based on hostname

--- src/pages/baseurl/apiDomain.js
+++ src/pages/baseurl/apiDomain.js
@@ -1,13 +1,32 @@
-// const baseURL = "https://panchshil-super.lockated.com/";
-const baseURL = "https://dev-panchshil-super-app.lockated.com/";
-// const baseURL = "https://api-connect.panchshil.com/"
-// const baseURL = "http://localhost:3000/";
-// const baseURL = "https://piramal-loyalty-dev.lockated.com/";
+let baseURL = "";
 
+const hostname = window.location.hostname;
+
+switch (hostname) {
+  case "ui-panchshil-super.lockated.com":
+    baseURL = "https://api-connect.panchshil.com";
+    break;
+
+  case "ui-loyalty-super.lockated.com":
+    baseURL = "https://dev-panchshil-super-app.lockated.com/";
+    break;
+
+  case "localhost":
+    baseURL = "http://localhost:3000/";
+    break;
+
+  default:
+    baseURL = "https://dev-panchshil-super-app.lockated.com/"; // fallback
+    break;
+}
 
 const token = localStorage.getItem("access_token");
-const LOGO_URL = "https://panchshil.gophygital.work/uploads/pms/company_setup/logo/226/Panchshil_logo.png";
-const Rustomji_URL = "https://lockated-public.s3.ap-south-1.amazonaws.com/attachfiles/documents/116/original/Rustomjee_Logo-02_1.png";
-const Rustomji_URL_Black = "https://lockated-public.s3.ap-south-1.amazonaws.com/attachfiles/documents/158/original/Rustomjee_Logo-01_2.png";
 
-export { baseURL, token, LOGO_URL ,Rustomji_URL , Rustomji_URL_Black};
+const LOGO_URL =
+  "https://panchshil.gophygital.work/uploads/pms/company_setup/logo/226/Panchshil_logo.png";
+const Rustomji_URL =
+  "https://lockated-public.s3.ap-south-1.amazonaws.com/attachfiles/documents/116/original/Rustomjee_Logo-02_1.png";
+const Rustomji_URL_Black =
+  "https://lockated-public.s3.ap-south-1.amazonaws.com/attachfiles/documents/158/original/Rustomjee_Logo-01_2.png";
+
+export { baseURL, token, LOGO_URL, Rustomji_URL, Rustomji_URL_Black };
